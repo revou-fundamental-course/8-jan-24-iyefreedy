fcb9776b7d54a01719cc986b8ed32b8affb1ea3a
Muhammad Quraisy <quraisy@uai.ac.id>
2024-01-16 16:06:27 +0700

    add interactivity

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
-const shapeImageCanvasElement = document.querySelector("#shapeImage");
+const title = document.querySelector("header span");
+const canvas = document.querySelector("#shapeImage");
+const calculatorForm = document.querySelector("#calculatorForm");
+const perimeterResultElement = document.querySelector(
+    ".result-wrapper #perimeterResult span"
+);
+const areaResultElement = document.querySelector(
+    ".result-wrapper #areaResult span"
+);
+const submitButtonElement = document.querySelector("#calculatorForm button");
 
 document.addEventListener("DOMContentLoaded", function () {
-    const ctx = shapeImageCanvasElement.getContext("2d");
+    const ctx = canvas.getContext("2d");
 
     // Gambar persegi di dalam canvas
     ctx.fillStyle = "lightGray";
-    ctx.fillRect(
-        0,
-        0,
-        shapeImageCanvasElement.width,
-        shapeImageCanvasElement.height
-    );
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     // Tambahkan petunjuk setiap sisinya
-    ctx.font = "14px Arial";
+    ctx.font = "bold 14px Arial";
     ctx.fillStyle = "black";
-    ctx.fillText("s", shapeImageCanvasElement.width / 2 - 5, 12);
-    ctx.fillText(
-        "s",
-        shapeImageCanvasElement.width - 12,
-        shapeImageCanvasElement.height / 2 + 5
-    );
-    ctx.fillText(
-        "s",
-        shapeImageCanvasElement.width / 2 - 5,
-        shapeImageCanvasElement.height - 5
-    );
-    ctx.fillText("s", 5, shapeImageCanvasElement.height / 2 + 5);
+    ctx.fillText("s", canvas.width / 2 - 5, 12);
+    ctx.fillText("s", canvas.width - 12, canvas.height / 2 + 5);
+    ctx.fillText("s", canvas.width / 2 - 5, canvas.height - 5);
+    ctx.fillText("s", 0, canvas.height / 2 + 5);
+
+    calculatorForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        submitButtonElement.textContent = "Calculating...";
+
+        setTimeout(() => {
+            const sideValue = document.querySelector("input[name=side]").value;
+
+            const perimeterResult = sideValue * 4;
+            const areaResult = sideValue * sideValue;
+
+            perimeterResultElement.textContent = `${sideValue} x 4 = ${perimeterResult}`;
+            areaResultElement.textContent = `${sideValue} x ${sideValue} = ${areaResult}`;
+
+            calculatorForm.reset();
+            submitButtonElement.textContent = "Hitung";
+        }, 2000);
+    });
 });
